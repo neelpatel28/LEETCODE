d50c175645b783fb3935a246ae31826d1713b7bd
Neel Patel <neelamrishpatel@gmail.com>
2026-05-03 20:39:52 +0530

    03.05.26

---
+++ 796_Rotate_String.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def rotateString(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        if len(s) != len(goal):
+            return False
+        
+        return goal in (s + s) #goal must be a substring of s+s to be a rotation of s
