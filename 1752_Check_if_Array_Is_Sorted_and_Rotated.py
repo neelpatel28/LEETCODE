9dd439ed42827e9ba29f1174197d995f574cda61
Neel Patel <neelamrishpatel@gmail.com>
2026-05-23 18:48:47 +0530

    23.05.26

---
+++ 1752_Check_if_Array_Is_Sorted_and_Rotated.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def check(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        n = len(nums)
+        count = 0
+        
+        for i in range(n):
+            if nums[i] > nums[(i + 1) % n]:
+                count += 1
+                if count > 1:
+                    return False
+        
+        return True
