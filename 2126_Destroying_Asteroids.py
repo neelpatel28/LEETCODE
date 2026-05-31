9532808cb3226b683424d9a20907bef7aa1018fd
Neel Patel <neelamrishpatel@gmail.com>
2026-05-31 17:23:21 +0530

    31.05.26

--- 2126_Destroying_Asteroids.py
+++ 2126_Destroying_Asteroids.py
@@ -1,12 +1,16 @@
 class Solution(object):
     def asteroidsDestroyed(self, mass, asteroids):
+        """
+        :type mass: int
+        :type asteroids: List[int]
+        :rtype: bool
+        """
         asteroids.sort()
         
-        curr_mass = mass  # local variable
-        
-        for a in asteroids:
-            if curr_mass < a:
+        for asteroid in asteroids:
+            if mass >= asteroid:
+                mass += asteroid
+            else:
                 return False
-            curr_mass += a
         
         return True
