107baf36632ef5ff67514021d8ae44bc82acb396
Neel Patel <neelamrishpatel@gmail.com>
2026-06-24 22:29:13 +0530

    20.06.26

--- 1840_Maximum_Building_Height.py
+++ 1840_Maximum_Building_Height.py
@@ -1,27 +1,30 @@
-class Solution:
+class Solution(object):
     def maxBuilding(self, n, restrictions):
-        if not restrictions:
-            return n - 1
-        
+        """
+        :type n: int
+        :type restrictions: List[List[int]]
+        :rtype: int
+        """
+        restrictions.sort()
         points = [[1, 0]] + restrictions
-        points.sort()
         
         for i in range(1, len(points)):
-            max_from_left = points[i - 1][1] + (points[i][0] - points[i - 1][0])
+            dist = points[i][0] - points[i-1][0]
+            max_from_left = points[i-1][1] + dist
             points[i][1] = min(points[i][1], max_from_left)
         
         for i in range(len(points) - 2, -1, -1):
-            max_from_right = points[i + 1][1] + (points[i + 1][0] - points[i][0])
+            dist = points[i+1][0] - points[i][0]
+            max_from_right = points[i+1][1] + dist
             points[i][1] = min(points[i][1], max_from_right)
         
         max_height = 0
         for i in range(len(points) - 1):
-            distance = points[i + 1][0] - points[i][0]
-            peak = (points[i][1] + points[i + 1][1] + distance) // 2
+            dist = points[i+1][0] - points[i][0]
+            peak = (points[i][1] + points[i+1][1] + dist) // 2
             max_height = max(max_height, peak)
         
-        last_pos, last_height = points[-1]
-        if last_pos < n:
-            max_height = max(max_height, last_height + (n - last_pos))
+        if points[-1][0] < n:
+            max_height = max(max_height, points[-1][1] + (n - points[-1][0]))
         
         return max_height
