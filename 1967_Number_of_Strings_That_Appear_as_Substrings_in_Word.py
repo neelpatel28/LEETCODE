ce4a0991e28bdf3e8a4bff826bec33ad2fbc5cec
Neel Patel <neelamrishpatel@gmail.com>
2026-06-29 21:19:45 +0530

    29.06.26

---
+++ 1967_Number_of_Strings_That_Appear_as_Substrings_in_Word.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def numOfStrings(self, patterns, word):
+        """
+        :type patterns: List[str]
+        :type word: str
+        :rtype: int
+        """
+        return sum(pattern in word for pattern in patterns)
